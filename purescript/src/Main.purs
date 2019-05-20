f2456a9d8d7f9974a37f63f8dab8608a9d232418
Alexander Konovalov <alex.knvl@gmail.com>
2019-05-20 03:10:44 -0700

    More purescript.

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -7,6 +7,8 @@ import Effect.Console (log)
 import Control.Monad.State.Trans
 import Effect.Class (liftEffect)
 
+import Data.Lazy (Lazy, force)
+import Control.Lazy
 import Data.Maybe
 import Data.Tuple
 import Data.Traversable
@@ -25,50 +27,64 @@ instance nameHashable :: Hashable (Name a) where
 class Functor f <= Recursive t f | t -> f where
   embed   :: f t -> t
   project :: t -> f t
-  cata :: forall z. (f z -> z) -> t -> z
 
-main :: Effect Unit
-main = do
-  log "Hello sailor!"
+cata :: forall f t z. Recursive t f => (f z -> z) -> t -> z
+cata phi = let go x = phi (go <$> project x) in go
 
 newtype Graph f = Graph (HM.HashMap Int (f Int))
 
-data State t f = State {
+type State t f = {
   size  :: Int,
   names :: HM.HashMap (Name t) Int,
   graph :: HM.HashMap Int (f Int)
 }
 
 emptyState :: forall t f. State t f
-emptyState = State { size: 0, names: HM.empty, graph: HM.empty }
+emptyState = { size: 0, names: HM.empty, graph: HM.empty }
 
 toGraph :: forall t f. Recursive t f => Traversable f => t -> Effect (Graph f)
-toGraph node = Graph <$> (\x -> x.graph) <$> snd <$> runStateT (go node) emptyState where
-  go :: Recursive t f => Traversable f => t -> StateT (State t f) Effect Int
-  go node = do
-    name <- liftEffect (Name <$> makeStableName node)
-    oldState <- get
+toGraph node = Graph <$> (\x -> x.graph) <$> snd <$> runStateT (toGraph' node) emptyState
 
-    case HM.lookup name oldState.names of
-      Just key -> pure key
-      Nothing  -> do
-        modify $ \s -> s {
-          size = oldState.size + 1,
-          names = HM.insert name oldState.size oldState.names
-        }
+toGraph' :: forall t f. Recursive t f => Traversable f => t -> StateT (State t f) Effect Int
+toGraph' node = do
+  name <- liftEffect (Name <$> makeStableName node)
+  oldState <- get
 
-        entry <- traverse go $ project node
-        modify $ \s -> s { graph = HM.insert oldState.size entry s.graph }
-        pure oldState.size
+  case HM.lookup name oldState.names of
+    Just key -> pure key
+    Nothing  -> do
+      _ <- modify $ \s -> s {
+        size = oldState.size + 1,
+        names = HM.insert name oldState.size oldState.names
+      }
 
-fromGraph :: Recursive t f => Traversable f => Graph f -> t
-fromGraph (Graph m) = go 0 where
-  go i = case HM.lookup i m of
-    Just fa -> embed <$> go fa
+      entry <- traverse toGraph' $ project node
+      _ <- modify $ \s -> s { graph = HM.insert oldState.size entry s.graph }
+      pure oldState.size
 
-data Node = Delay Node | Not Node
+fromGraph :: forall t f. Partial => Recursive t f => Traversable f => Graph f -> t
+fromGraph g = fromGraph' g 0
 
-data NodeF a = DelayF a | NotF a
+fromGraph' :: forall t f. Partial => Recursive t f => Traversable f => Graph f -> Int -> t
+fromGraph' (Graph m) i = case HM.lookup i m of
+  Just fa -> embed (fromGraph' (Graph m) <$> fa)
 
--- derive instance Functor
--- derive instance Foldable, Traversable)
+-- data Node = Delay (Lazy Node) | Not Node
+
+-- data NodeF a = DelayF a | NotF a
+
+-- derive instance nodeFunctor :: Functor NodeF
+-- -- derive instance Foldable, Traversable)
+
+-- instance nodeRecursive :: Recursive Node NodeF where
+--   embed (DelayF a) = Delay (defer $ \_ -> a)
+--   embed (NotF a)   = Not a
+
+--   project (Delay a) = DelayF (force a)
+--   project (Not a)   = NotF a
+
+-- network = fix \n -> Not $ Delay (defer $ \_ -> n)
+
+main :: Effect Unit
+main = do
+  log "Hello sailor!"
